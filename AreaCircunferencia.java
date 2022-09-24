8099992afb29dd0dcc61efb217c042a23c930e75
Newton Lomar <newtonlomar16@gmail.com>
2022-09-24 18:42:30 -0300

    adding solutin to exercise AreaCircunferencia

---
+++ AreaCircunferencia.java
@@ -0,0 +1,10 @@
+/* 3) Implemente a Classe AreaCircunferencia que imprima a área de uma circunferência de raio 10. */
+
+public class AreaCircunferencia {
+
+	public static void main(String[] args) {
+		double areaCircunferenciaRaioDez = Math.PI * Math.pow(10, 2);
+		System.out.printf("Area da circunferencia: %.1f", areaCircunferenciaRaioDez);
+	}
+
+}
